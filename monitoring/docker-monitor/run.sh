2bfa0f27f588d731706a94699ee99d87e1530905
abijithanikkuruthi <abijithanikkuruthi@gmail.com>
2022-05-27 17:09:10 +0200

    docker monitoring and job log

--- monitoring/docker-monitor/run.sh
+++ monitoring/docker-monitor/run.sh
@@ -1,13 +1,20 @@
-mkdir /logs
+mkdir -p /logs
 cd /logs
-rm -rf *
 
-echo "TIMESTAMP                 CONTAINER_ID   NAME                        CPU %     MEM USAGE / LIMIT     MEM %     NET I/O           BLOCK I/O         PIDS" > `date -u +"%Y%m%d.txt"`
+foldername=`date`
+mkdir "$foldername"
+cd "$foldername"
+
+echo "TIMESTAMP                       CONTAINER_ID   NAME                        CPU %     MEM USAGE / LIMIT     MEM %     NET I/O           BLOCK I/O         PIDS" > docker_monitor.log
 
 while true;
 do
+    # DOCKER STATS
     docker stats --no-stream | tail -n +2 | while read line ; do
-        echo -e `date`'\t'$line >> `date -u +"%Y%m%d.txt"`
+        echo -e `date`'\t'$line >> docker_monitor.log
     done
+
+    # JOB LOG API
+    wget --timeout=5 --tries=1 -q -O job_log.log $JOB_LOG_API_URL #> /dev/null 2>&1
     sleep 10
 done
