f541c7d3952c95eee3f4b9d2441fbdaeb108b8f9
abijithanikkuruthi <abijithanikkuruthi@gmail.com>
2022-06-06 11:23:00 +0200

    added boolean check before setup

--- message-database/messenger/main.py
+++ message-database/messenger/main.py
@@ -4,23 +4,26 @@ from common import create_topics, create_database, create_table
 from constants import *
 
 def setup():
-    # Kafka Setup
-    create_topics([{
-        'name' : KAFKA_MESSAGE_TOPIC,
-        'num_partitions' : KAFKA_MESSAGE_TOPIC_PARTITIONS,
-        'retention' : EXPERIMENT_DURATION_HOURS * 60 * 60
-    }])
 
-    # Database Scheduler Setup
-    create_database(DATABASE_SCHEDULER_DATABASE)
+    if KAFKA_ENABLED:
+        # Kafka Setup
+        create_topics([{
+            'name' : KAFKA_MESSAGE_TOPIC,
+            'num_partitions' : KAFKA_MESSAGE_TOPIC_PARTITIONS,
+            'retention' : EXPERIMENT_DURATION_HOURS * 60 * 60
+        }])
 
-    messages_table_schema = ""
-    for k, v in MESSAGES_TABLE_SCHEMA.items():
-        messages_table_schema += f"`{k}` {v}, "
-    
-    messages_table_schema = messages_table_schema[:-2]
-    
-    create_table(DATABASE_SCHEDULER_SM_TABLE, messages_table_schema)
+    if DATABASE_SCHEDULER_ENABLED:
+        # Database Scheduler Setup
+        create_database(DATABASE_SCHEDULER_DATABASE)
+
+        messages_table_schema = ""
+        for k, v in MESSAGES_TABLE_SCHEMA.items():
+            messages_table_schema += f"`{k}` {v}, "
+        
+        messages_table_schema = messages_table_schema[:-2]
+        
+        create_table(DATABASE_SCHEDULER_SM_TABLE, messages_table_schema)
 
 if __name__ == '__main__':
     setup()
