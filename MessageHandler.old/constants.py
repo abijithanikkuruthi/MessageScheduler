8638925fb84aa060f11d2a8464a362afbf799c05
abijithanikkuruthi <abijithanikkuruthi@gmail.com>
2022-06-06 11:15:12 +0200

    remove python message handler

--- MessageHandler.old/constants.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Enable debugging for detailed logs
-DEBUG = (os.getenv('DEBUG', 'False') == 'True')
-
-SCHEDULER_SERVER_URL = os.getenv('SCHEDULER_SERVER_URL', 'http://localhost:8000')
-REQUEST_ERROR_WAIT_TIME = int(os.getenv('REQUEST_ERROR_WAIT_TIME', '1'))
-REQUEST_COUNT_LIMIT = int(os.getenv('REQUEST_COUNT_LIMIT', '3'))
--- MessageHandler.old/setup.sh
+++
@@ -1,3 +0,0 @@
-apt update
-apt install -y librdkafka-dev
-pip3 install --no-cache-dir -r requirements.txt
